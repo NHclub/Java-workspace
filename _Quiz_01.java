df59083e596dd328c3cbb0e29c38b5e6785fccb1
hh <sbe4488@naver.com>
2023-03-22 15:26:15 +0900

    02: 자료형 03: 변수선언 04: 주석 05: 적절한 변수명 선언 06: 상수 07. 형변환 Quiz1. 적절한 자료형을 변수에 저장하기.

---
+++ _Quiz_01.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        /*Quiz 1. 버스 도착 정보를 출력하는 프로그램을 작성하시오
+                각 정보는 적절한 자료형의 변수에 정의합니다.*/
+        String busName = "상암08";
+        System.out.println(busName + "번 버스");
+        int minute = 3;
+        System.out.println("약 " + minute + "분 후 도착");
+        float distance = 1.2f;
+        System.out.println("남은 거리 " + distance + "km");
+    }
+}
